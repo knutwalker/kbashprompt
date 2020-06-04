7bef82d1806005889519780717d4d17ceb63fa38
Paul Horn <developer@knutwalker.de>
2020-06-04 19:10:36 +0200

    Add info about SDK versions if in git repo

--- src/main.rs
+++ src/main.rs
@@ -82,6 +82,7 @@ fn current_dir(f: &mut fmt::Formatter<'_>) -> fmt::Result {
 
 fn git_prompt(f: &mut fmt::Formatter<'_>) -> fmt::Result {
     if let Ok(repo) = Repository::open_from_env() {
+        repo_type(&repo, f).unwrap_or(Ok(()))?;
         branch_name(&repo, f)?;
         repo_changes(&repo, f).unwrap_or(Ok(()))?;
         repo_state(&repo, f)?;
@@ -202,4 +203,67 @@ fn repo_changes(repo: &Repository, f: &mut fmt::Formatter<'_>) -> Option<fmt::Re
     };
 
     Some(write!(f, " {}", buf.color(color)))
+}
+
+fn repo_type(repo: &Repository, f: &mut fmt::Formatter<'_>) -> Option<fmt::Result> {
+    let wd = repo.workdir()?;
+    if let Some(Err(e)) = rust_version(wd, f) {
+        return Some(Err(e));
+    }
+    if let Some(Err(e)) = java_version(wd, f) {
+        return Some(Err(e));
+    }
+
+    Some(Ok(()))
+}
+
+fn rust_version(wd: impl AsRef<Path>, f: &mut fmt::Formatter<'_>) -> Option<fmt::Result> {
+    let cargo_toml = wd.as_ref().join("Cargo.toml");
+    let cargo_toml = cargo_toml.metadata().ok()?;
+    if !cargo_toml.is_file() {
+        return None;
+    }
+
+    let rustc_version = Command::new("rustc").arg("--version").output().ok()?;
+    if !rustc_version.status.success() {
+        return None;
+    }
+
+    let rustc_version = String::from_utf8(rustc_version.stdout).ok()?;
+    let rustc_version = rustc_version.split_whitespace().nth(1)?;
+
+    Some(write!(f, " 🦀 {}", rustc_version.color(ORANGE)))
+}
+
+fn java_version(wd: impl AsRef<Path>, f: &mut fmt::Formatter<'_>) -> Option<fmt::Result> {
+    let has_file = move |file: &str| -> Option<()> {
+        let build_file = wd.as_ref().join(file);
+        build_file
+            .metadata()
+            .ok()
+            .filter(|f| f.is_file())
+            .map(|_| ())
+    };
+
+    for file in [
+        "build.gradle",
+        "build.sbt",
+        "pom.xml",
+        "build.gradle.kts",
+        "build.xml",
+    ]
+    .iter()
+    {
+        if has_file(file).is_some() {
+            let java_home = env::var_os("JAVA_HOME")?;
+            let java_home = PathBuf::from(java_home);
+            let java_home = java_home.read_link().unwrap_or(java_home);
+            let java_home = java_home.file_name()?;
+            let java_home = java_home.to_string_lossy();
+
+            return Some(write!(f, " ☕️ {}", java_home.color(CYAN)));
+        }
+    }
+
+    None
 }
