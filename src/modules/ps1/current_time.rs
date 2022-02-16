1ee1a9ade69416a3485f337b9c58614e05999995
Paul Horn <dev@knutwalker.engineer>
2022-02-16 23:32:50 +0100

    Update dependencies

--- src/modules/ps1/current_time.rs
+++ src/modules/ps1/current_time.rs
@@ -3,7 +3,7 @@ use colorful::Style;
 use time::OffsetDateTime;
 
 pub(super) fn info() -> ModuleInfo {
-    let now = OffsetDateTime::now_local().time();
+    let now = OffsetDateTime::now_local().unwrap().time();
     let now = format!("{:02}:{:02}:{:02}", now.hour(), now.minute(), now.second());
     ModuleInfo::of().info(now).style(Style::Dim)
 }
